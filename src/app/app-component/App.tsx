00f1082de54f791ebb78c102a9e5949c1e7df039
danyabdo94 <dany.kamel@zyda.com>
2021-06-25 15:06:05 +0200

    Add a home page with router

--- src/app/app-component/App.tsx
+++ src/app/app-component/App.tsx
@@ -1,7 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "../../pages";
 
 function App(): JSX.Element {
-    return <ChakraProvider></ChakraProvider>;
+    return (
+        <ChakraProvider>
+            <Router>
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </ChakraProvider>
+    );
 }
 
 export default App;
