5c4aa9c79bd0a57349ec798fdd28bb76817a1c18
danyabdo94 <dany.kamel@zyda.com>
2021-06-26 17:30:44 +0200

    Add stepper UI

--- src/app/app-component/App.tsx
+++ src/app/app-component/App.tsx
@@ -1,10 +1,17 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Main } from "../../components";
+import { StepsStyleConfig as Steps } from "chakra-ui-steps";
+
+const theme = extendTheme({
+    components: {
+        Steps,
+    },
+});
 
 function App(): JSX.Element {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Router>
                 <Switch>
                     <Route path="/">
--- src/components/employees-table/employees-table.tsx
+++ src/components/employees-table/employees-table.tsx
@@ -1,11 +1,20 @@
-import { Table, Thead, Tbody, Tr, Th, Td, Flex, IconButton, Avatar, Box } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td, Flex, IconButton } from "@chakra-ui/react";
 import { ViewIcon } from "@chakra-ui/icons";
 import { useTranslation } from "react-i18next";
 import mockedEmployees from "../../app/mock.data";
+import { Step, Steps, useSteps } from "chakra-ui-steps";
 
 export default function EmployeesTable(): JSX.Element {
     const { t } = useTranslation("employeesTable");
-
+    const content = <Flex py={4}>Test</Flex>;
+    const steps = [
+        { label: "Step 1", content },
+        { label: "Step 2", content },
+        { label: "Step 3", content },
+    ];
+    const { nextStep, prevStep, reset, activeStep } = useSteps({
+        initialStep: 0,
+    });
     return (
         <Flex my={2} mx={4}>
             <Table variant="simple">
@@ -24,7 +33,21 @@ export default function EmployeesTable(): JSX.Element {
                             <Td>{employee.id}</Td>
                             <Td>{`${employee.name}, ${employee.countryCode}`}</Td>
                             <Td>{employee.position}</Td>
-                            <Td>{employee.state}</Td>
+                            <Td>
+                                <Steps activeStep={activeStep}>
+                                    {steps.map(({ label, content }) => (
+                                        <Step
+                                            label={label}
+                                            key={label}
+                                            onClick={(e) => {
+                                                console.log(e);
+                                            }}
+                                        >
+                                            {content}
+                                        </Step>
+                                    ))}
+                                </Steps>
+                            </Td>
                             <Td>
                                 <IconButton
                                     variant="outline"
